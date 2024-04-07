142d7c9cea80685122d4d6a56fe8d0ad5ecfd6fd
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-07 04:14:21 +0300

    Modifying the tile to accept data from homepage

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ToDoTile extends StatelessWidget {
-  const ToDoTile({super.key});
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile({super.key, required this.taskName, required this.taskCompleted});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: Container(
+        padding: const EdgeInsets.all(24.0),
+        decoration: BoxDecoration(
+            color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          children: [
+            Checkbox(value: taskCompleted, onChanged: onChanged),
+            Text(taskName),
+          ],
+        ),
+      ),
+    );
   }
 }

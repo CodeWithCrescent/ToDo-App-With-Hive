07dc95c2d9cc9df1e86f86cc5023267185369ce4
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-20 15:46:06 +0300

    Added the slidable action for delete button on sliding from right to left

--- lib/widgets/todo_tile.dart
+++ lib/widgets/todo_tile.dart
@@ -1,26 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ToDoTile extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
   Function(bool?)? onChanged;
+  Function(BuildContext)? deleteFunction;
 
-  ToDoTile({super.key, required this.taskName, required this.taskCompleted});
+  ToDoTile({
+    super.key,
+    required this.taskName,
+    required this.taskCompleted,
+    this.onChanged,
+    required this.deleteFunction,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(25.0),
-      child: Container(
-        padding: const EdgeInsets.all(24.0),
-        decoration: BoxDecoration(
-            color: Colors.yellow, borderRadius: BorderRadius.circular(12)),
-        child: Row(
-          children: [
-            Checkbox(value: taskCompleted, onChanged: onChanged),
-            Text(taskName),
-          ],
+      padding: const EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
+      child: Slidable(
+        endActionPane: ActionPane(motion: const StretchMotion(), children: [
+          SlidableAction(
+            onPressed: deleteFunction,
+            icon: Icons.delete,
+            backgroundColor: Colors.red,
+            borderRadius: BorderRadius.circular(12),
+          )
+        ]),
+        child: Container(
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChanged,
+                activeColor: Colors.black,
+              ),
+              Text(
+                taskName,
+                style: TextStyle(
+                  fontSize: 16,
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

a8e998516b96188bc77c86fcdc865b7b2c2f7928
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-20 15:51:03 +0300

    Creating the Hive database storage for the todos

---
+++ lib/data/database.dart
@@ -0,0 +1,27 @@
+import 'package:hive_flutter/hive_flutter.dart';
+
+class ToDoDataBase {
+  List toDoList = [];
+
+  // reference the box
+  final _myBox = Hive.box('mybox');
+
+  // run this method for first time oprnning this app
+  void createInitialData() {
+    toDoList = [
+      ['Make tutorial', false],
+      ['Go dinner', false],
+      ['Trial tasks', false],
+    ];
+  }
+
+  // load the data from database
+  void loadData() {
+    toDoList = _myBox.get('TODOLIST');
+  }
+
+  // update the database
+  void updateDataBase() {
+    _myBox.put('TODOLIST', toDoList);
+  }
+}

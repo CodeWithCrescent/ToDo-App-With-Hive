9510dc773565096a263a400072454799dfa0aaa3
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-20 15:45:01 +0300

    Initialized the hive data storage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_hive/data/database.dart';
+import 'package:todo_hive/widgets/dialog_box.dart';
 import 'package:todo_hive/widgets/todo_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // reference the box
+  final _myBox = Hive.box('mybox');
+  // a db instance of a ToDoDataBase() class
+  ToDoDataBase db = ToDoDataBase();
+
+  @override
+  void initState() {
+    // if this is the first time ever opening the app, then create default data
+    if (_myBox.get('TODOLIST') == null) {
+      db.createInitialData();
+    } else {
+      // there already exists some data
+      db.loadData();
+    }
+
+    super.initState();
+  }
+
+  // text Controller from dialog box
+  final _controller = TextEditingController();
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      db.toDoList[index][1] = !db.toDoList[index][1];
+    });
+    db.updateDataBase();
+  }
+
+  void saveNewTask() {
+    setState(() {
+      db.toDoList.add([_controller.text, false]);
+      _controller.clear();
+      Navigator.of(context).pop();
+    });
+    db.updateDataBase();
+  }
+
+  void createNewTask() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return DialogBox(
+            controller: _controller,
+            onSave: saveNewTask,
+            onCancel: () {
+              _controller.clear();
+              Navigator.of(context).pop();
+            },
+          );
+        });
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      db.toDoList.removeAt(index);
+    });
+    db.updateDataBase();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,21 +78,22 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('TO DO'),
       ),
-      body: ListView(
-        children: [
-          ToDoTile(
-            taskName: 'A test task',
-            taskCompleted: true,
-          ),
-          ToDoTile(
-            taskName: 'Hello tuesday',
-            taskCompleted: false,
-          ),
-          ToDoTile(
-            taskName: 'Wake up early',
-            taskCompleted: false,
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        backgroundColor: Colors.yellow,
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        physics: const BouncingScrollPhysics(),
+        itemCount: db.toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: db.toDoList[index][0],
+            taskCompleted: db.toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+            deleteFunction: (contex) => deleteTask(index),
+          );
+        },
       ),
     );
   }

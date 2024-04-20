50dbf34fc8c3787179e8e9a67250832e0dcca6ae
Crescent Geniius <crescentbeatz31@gmail.com>
2024-04-20 16:43:55 +0300

    Added the utils/ folder for utilities like colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,10 @@
+import 'package:hexcolor/hexcolor.dart';
+
+class GlobalColors {
+  static HexColor mainColor = HexColor('#5a55ca');
+  static HexColor bgColor = HexColor('#f0f4fd');
+  static HexColor whiteTextColor = HexColor('#fdfdfd');
+  static HexColor whiteColor = HexColor('#ffffff');
+  static HexColor blackColor = HexColor('#121128');
+  static HexColor redColor = HexColor('#ca5a55');
+}
